7b5f565995f95ff55d881e1a5b96adf4b30ff717
Christopher Hill <chris@massdriver.cloud>
2023-05-14 15:57:01 -0600

    Add access logging (on a conditional) for HIPAA compliance (#15)

--- src/main.tf
+++ src/main.tf
@@ -3,9 +3,18 @@ resource "aws_s3_bucket" "main" {
   force_destroy = false
 }
 
-resource "aws_s3_bucket_acl" "private" {
+# Resource to avoid error "AccessControlListNotSupported: The bucket does not allow ACLs"
+resource "aws_s3_bucket_ownership_controls" "main" {
   bucket = aws_s3_bucket.main.id
-  acl    = "private"
+  rule {
+    object_ownership = "ObjectWriter"
+  }
+}
+
+resource "aws_s3_bucket_acl" "private" {
+  bucket     = aws_s3_bucket.main.id
+  acl        = "private"
+  depends_on = [aws_s3_bucket_ownership_controls.main]
 }
 
 resource "aws_s3_bucket_public_access_block" "main" {
@@ -14,4 +23,60 @@ resource "aws_s3_bucket_public_access_block" "main" {
   block_public_acls   = true
   block_public_policy = true
   ignore_public_acls  = true
+}
+
+
+// Access Logging
+resource "aws_s3_bucket" "access_logs" {
+  count  = var.monitoring.access_logging ? 1 : 0
+  bucket = "${var.md_metadata.name_prefix}-logging"
+}
+
+# Resource to avoid error "AccessControlListNotSupported: The bucket does not allow ACLs"
+resource "aws_s3_bucket_ownership_controls" "access_logs" {
+  count  = var.monitoring.access_logging ? 1 : 0
+  bucket = aws_s3_bucket.access_logs.0.id
+  rule {
+    object_ownership = "ObjectWriter"
+  }
+}
+
+resource "aws_s3_bucket_acl" "access_logs" {
+  count      = var.monitoring.access_logging ? 1 : 0
+  bucket     = aws_s3_bucket.access_logs.0.id
+  acl        = "log-delivery-write"
+  depends_on = [aws_s3_bucket_ownership_controls.access_logs]
+}
+
+resource "aws_s3_bucket_public_access_block" "access_logs" {
+  count  = var.monitoring.access_logging ? 1 : 0
+  bucket = aws_s3_bucket.access_logs.0.id
+
+  block_public_acls   = true
+  block_public_policy = true
+  ignore_public_acls  = true
+}
+
+resource "aws_s3_bucket_logging" "main" {
+  count  = var.monitoring.access_logging ? 1 : 0
+  bucket = aws_s3_bucket.main.id
+
+  target_bucket = aws_s3_bucket.access_logs.0.id
+  target_prefix = "log/"
+}
+
+resource "aws_s3_bucket_lifecycle_configuration" "access_logs" {
+  count  = var.monitoring.access_logging ? 1 : 0
+  bucket = aws_s3_bucket.access_logs.0.id
+
+  rule {
+    id = "lifecycle"
+
+    status = "Enabled"
+
+    transition {
+      days          = 7
+      storage_class = "INTELLIGENT_TIERING"
+    }
+  }
 }
